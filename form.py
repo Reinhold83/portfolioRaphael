fb5b7119a19674f8d6ad41edcb4cd389003189e7
Reinhold83 <raphaelreinhold@hotmail.com>
2020-08-17 12:14:12 +0100

    contactdone_

--- form.py
+++ form.py
@@ -1,11 +1,11 @@
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms import TextField, SubmitField, IntegerField, StringField, TextAreaField
 from wtforms.validators import DataRequired, Email, InputRequired
 from wtforms.fields.html5 import EmailField
 from flask import flash
 
  
-class ContactForm(Form):
+class ContactForm(FlaskForm):
     name = StringField("Enter your name please", validators=[InputRequired()], render_kw={'placeholder':'Enter your name'})
     email = EmailField('Enter your email address', validators=[InputRequired(), Email('This field requires a valid email address')], render_kw={'placeholder':'Enter your email'})
     subject = StringField("Subject", validators=[InputRequired()], render_kw={'placeholder':'Enter subject'})
